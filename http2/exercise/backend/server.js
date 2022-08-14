b099d39d03f64440bc62cd58914c0ea65a4e6943
Michael McCarthy <mike@pixelonezero.com>
2022-08-14 14:23:48 -0700

    Adding changes for completed polling and http2 exercises

--- http2/exercise/backend/server.js
+++ http2/exercise/backend/server.js
@@ -35,6 +35,31 @@ const server = http2.createSecureServer({
  *
  */
 
+// stream event is specific to HTTP2. stream event occurs, then request event, in that order for each http request.
+server.on("stream", (stream, headers) => {
+  const path = headers[":path"];
+  const method = headers[":method"];
+
+  // streams open for every request from the browser
+  if (path === '/msgs' && method === "GET") {
+    //immediately reply with 200 OK and the encoding
+    console.log('connected a stream' + stream.id);
+    stream.respond({
+      ":status": 200,
+      "content-type": "text/plain; charset=utf-8"
+    });
+
+    // write the first response
+    stream.write(JSON.stringify({ msg: getMsgs() }));
+    connections.push(stream);
+
+    stream.on("close", () => {
+      console.log("disconnected " + stream.id);
+      connections = connections.filter(s => s !== stream);
+    });
+  }
+});
+
 server.on("request", async (req, res) => {
   const path = req.headers[":path"];
   const method = req.headers[":method"];
@@ -53,11 +78,17 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({
+      user,
+      text,
+      time: Date.now()
+    });
+
+    res.end();
+
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msg: getMsgs() }));
+    });
   }
 });
 
--- http2/exercise/frontend/http2-chat.js
+++ http2/exercise/frontend/http2-chat.js
@@ -32,11 +32,43 @@ async function postNewMsg(user, text) {
 }
 
 async function getNewMsgs() {
-  /*
-   *
-   * code goes here
-   *
-   */
+  let reader;
+  const utf8Decoder = new TextDecoder("utf-8");
+  try {
+    const res = await fetch("/msgs");
+    reader = res.body.getReader();
+  } catch (e) {
+    console.log("connection error", e);
+  }
+  presence.innerText = "🟢";
+
+  let readerResponse;
+  let done;
+  do {
+    try {
+      readerResponse = await reader.read();
+    } catch (e) {
+      console.error("reader fail", e);
+      presence.innerText = "🔴";
+      return;
+    }
+
+    const chunk = utf8Decoder.decode(readerResponse.value, { stream: true });
+    done = readerResponse.data;
+
+    if (chunk) {
+      // Best practice: put JSON.parse invocations inside try...catch
+      try {
+        const json = JSON.parse(chunk);
+        allChat = json.msg;
+        render();
+      } catch (e) {
+        console.error("parse error", e);
+      }
+    }
+
+  } while (!done);
+  presence.innerText = "🔴";
 }
 
 function render() {
